a628476db7469ca82eedab415af6ff10c5aa6cab
Jonas Tratz <jonastratz@yahoo.de>
2020-01-15 13:19:26 +0100

    aufgabe1 fast fertig

--- src/aufgabe1_fileTransfer/client/NetCopy.java
+++ src/aufgabe1_fileTransfer/client/NetCopy.java
@@ -1,5 +1,14 @@
 package aufgabe1_fileTransfer.client;
+
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Uebertraegt ein File zwischen zwei Hosts. Arbeitet als Sender oder
@@ -17,24 +26,34 @@ public class NetCopy {
     private static final int PORT = 5000;
 
     /**
-     * Hauptprogramm.
-     * Sendet oder empfaengt ein File.
+     * Hauptprogramm. Sendet oder empfaengt ein File.
      *
-     * @param args Kommandozeilenargument: Lokaler Pfadname (Sender) oder Hostname (Empfaenger).
-     * @throws IOException          wenn beim Zugriff aufs Filesystem oder aufs Netzwerk etwas schief geht.
+     * @param args Kommandozeilenargument: Lokaler Pfadname (Sender) oder Hostname
+     *             (Empfaenger).
+     * @throws IOException wenn beim Zugriff aufs Filesystem oder aufs Netzwerk
+     *                     etwas schief geht.
      */
     public static void main(String... args) throws IOException {
-        if(!send(args[0]))
+        if (!send(args[0]))
             receive(args[0]);
     }
 
     /**
      * Holt ein File vom Server.
+     * 
      * @param hostname Hostname des Servers, der ein File liefert.
-     * @throws IOException wenn beim Empfangen oder Schreiben des Files etwas schief geht.
+     * @throws IOException wenn beim Empfangen oder Schreiben des Files etwas schief
+     *                     geht.
      */
     public static void receive(String hostname) throws IOException {
-        //TODO
+        // TODO - Client der File erhält
+
+        Socket clientSocket = new Socket(hostname, PORT);
+        BufferedReader from = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.println(from.readLine());
+
+
     }
 
     /**
@@ -42,12 +61,35 @@ public class NetCopy {
      *
      * @param filename Pfadname eines lokalen Files.
      * @return true, wenn filename versendet wurde; false ansonsten.
-     * @throws IOException wenn filename existiert, aber beim Lesen oder Senden etwas schief ging.
+     * @throws IOException wenn filename existiert, aber beim Lesen oder Senden
+     *                     etwas schief ging.
      */
     public static boolean send(String filename) throws IOException {
-        //TODO
+        // TODO - Server der File sendet
+        try {
+            Socket serverSocket = new ServerSocket(PORT).accept();
+            PrintWriter to = new PrintWriter(new OutputStreamWriter(serverSocket.getOutputStream()));
 
+            try {
+                String fname = "src/aufgabe1_fileTransfer/server/"+filename;
+                File file = new File(fname);
+                Scanner sc = new Scanner(file);
 
-        return true;
+                while (sc.hasNext()) {
+                    to.write(sc.nextLine());
+                    to.flush();
+                }
+
+                return true;
+
+            } catch (Exception e) {
+                System.out.println("Cannot Read file:" + e);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Cannot create Server Socket" + e);
+        }
+
+        return false;
     }
 }
